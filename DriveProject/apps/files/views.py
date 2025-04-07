ed94768333d07db7136f651a86a4b99c9992d280
amir <amir1381basketball@gmail.com>
2025-04-07 11:38:12 +0330

    first commit: add FileList, FileDetail, FileShare views

--- DriveProject/apps/files/views.py
+++ DriveProject/apps/files/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from DriveProject.apps.files.serializers import FileSerializer
+
+
+class FileList(generics.ListCreateAPIView):
+    serializer_class = FileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = self.request.user.files.all()
+        folder_id = self.request.query_params.get('folder')
+        if folder_id:
+            queryset = queryset.filter(folder__id=folder_id)
+        return queryset
+
+
+class FileDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = FileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.request.user.files.all()
+
+
+class FileShare(generics.UpdateAPIView):
+    serializer_class = FileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.request.user.files.all()
+
