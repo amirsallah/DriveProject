c26839531f4e7ecbbf5b5027be356fe74c6fdc1e
amirsallah <amir1381basketball@gmail.com>
2025-04-09 02:30:55 +0330

    feat : add sanitize_filename fix_s3_url

--- DriveProject/apps/files/models.py
+++ DriveProject/apps/files/models.py
@@ -1,4 +1,6 @@
 import mimetypes
+import os
+import re
 
 from django.contrib.auth.models import User
 from django.db import models
@@ -35,18 +37,36 @@ class Folder(CreationModificationDateAbstractModel):
 
 class File(CreationModificationDateAbstractModel):
     name = models.CharField(max_length=255)
-    file = models.FileField(upload_to='uploads/')
+    file = models.FileField()
     folder = models.ForeignKey(Folder, null=True, blank=True, related_name='files', on_delete=models.CASCADE)
+    file_url = models.CharField(max_length=128, null=True, blank=True)
     owner = models.ForeignKey(User, related_name='files', on_delete=models.CASCADE)
     shared_with = models.ManyToManyField(User, related_name='shared_files', blank=True)
     size = models.BigIntegerField(null=True, blank=True)
     type = models.CharField(max_length=50, null=True, blank=True)
 
+    def fix_s3_url(self, url):
+        end_point_minio = "127.0.0.1:9000"
+        website_url = "192.168.123.105:9000"
+        url = str(url).split("?")[0]
+        url = url.replace(end_point_minio, f"{website_url}")
+        return url
+
+    def sanitize_filename(self, filename):
+        """ تغییر نام فایل برای حذف فاصله‌ها و کاراکترهای خاص """
+        name, ext = os.path.splitext(filename)
+        name = re.sub(r'\s+', '_', name)
+        name = re.sub(r'[^\w\-_.]', '', name)
+        return f"{name}{ext}"
+
     def save(self, *args, **kwargs):
         if self.file:
             self.size = self.file.size
-            mime_type, encoding = mimetypes.guess_type(self.file.name)
-            self.file_type = mime_type
+            # self.name = self.file.name
+            if self.file:
+                new_filename = self.sanitize_filename(self.file.name)
+                self.file.name = new_filename
+                self.name = new_filename[:128]
         super().save(*args, **kwargs)
 
     def add_user_to_share_list(self, user_id: int) -> None:
